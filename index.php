dc105b96f6dc90fcd533a4b764ee4b9cb53cfe03
Suwi D. Utomo <suwidadi@gmail.com>
2016-03-07 13:46:57 +0700

    change homepage

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+    <head>
+    	<title>Web Application Developer | Brainsoftmedia</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<meta content="Brainsoftmedia" name="description">
+		<meta content="Web, Media, Web Based Application, Web Application Developer, Sistem Informasi Rumah Sakit, Sistem Informasi, php, google, adwords" name="keywords">
+		<meta name="author" content="Suwi Dadi Utomo | Web Developer Apps" />
+		
+		<meta property="og:site_name" content="Brainsoftmedia">
+		<meta property="og:title" content="Brainsoftmedia">
+		<meta property="og:description" content="Official Website of Brainsoftmedia"/>
+		<meta property="og:type" content="website">
+		<meta property="og:image" content="http://www.brainsoftmedia.dev/assets/images/fb_hrd.jpg"><!-- link to image for socio -->
+		<meta property="og:image:width" content="285">
+		<meta property="og:image:height" content="216">
+		<meta property="og:url" content="http://www.brainsoftmedia.dev/">
+		
+		<link href="http://www.brainsoftmedia.dev/assets/avicon.ico" rel="shortcut icon"/>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"/> 
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.2.1/css/ripples.css" rel="stylesheet">
+        <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.2.1/css/material-wfont.min.css" rel="stylesheet">
+        <link href="assets/css/styles.css" rel="stylesheet">
+        <link href="assets/css/media.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <!-- Your site -->
+		<nav class="navbar navbar-default navbar-fixed-top shadow-z-1">
+			<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Brainsoftmedia</a>
+				</div>
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		<header class="header" style="margin-top:30px">
+	        <div class="container">                       
+	            <img class="profile-image img-responsive pull-left img-circle" src="assets/images/profile.jpg" alt="Suwi Dadi Utomo" />
+	            <div class="profile-content pull-left">
+	                <h1 class="name">Suwi Dadi Utomo</h1>
+	                <h2 class="desc">Web App Developer</h2>   
+	                <ul class="social list-inline">
+	                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>                   
+	                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	                    <li><a href="#"><i class="fa fa-github-alt"></i></a></li>                  
+	                    <li class="last-item"><a href="#"><i class="fa fa-hacker-news"></i></a></li>                 
+	                </ul> 
+	            </div><!--//profile-->
+	            <a class="btn btn-raised btn-material-teal pull-right" href="" target="_blank"><i class="fa fa-paper-plane"></i> Contact Me</a>              
+	        </div><!--//container-->
+	    </header><!--//header-->
+	    <div class="container sections-wrapper">
+	    	<div class="row">
+	    		<div class="primary col-xs-12 col-sm-12 col-md-8 col-lg-8">
+		    		<section class="about section">
+	                    <div class="section-inner">
+	                        <h2 class="heading">About Me</h2>
+	                        <div class="content">
+	                            <p>Write a brief intro about yourself. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec.</p>    
+	         					<p>It's a good idea to include your personal interests and hobbies as well. Commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+	         				</div><!--//content-->
+	                    </div><!--//section-inner-->                 
+	                </section><!--//section-->
+	    		</div>
+	    		<div class="secondary col-xs-12 col-sm-12 col-md-4 col-lg-4">
+	    			<aside class="info aside section">
+	    				<div class="section-inner">
+	                        <h2 class="heading sr-only">Basic Information</h2>
+	                        <div class="content">
+	                            <ul class="list-unstyled">
+	                                <li><i class="fa fa-map-marker"></i><span class="sr-only">Location:</span>Jakarta, ID</li>
+	                                <li><i class="fa fa-envelope-o"></i><span class="sr-only">Email:</span><a href="#">suwidadi@gmail.com</a></li>
+	                                <li><i class="fa fa-link"></i><span class="sr-only">Website:</span><a href="#">http://www.brainsoftmedia.com</a></li>
+	                            </ul>
+	                        </div><!--//content-->  
+	                    </div><!--//section-inner-->   
+	    			</aside>
+	    		</div>
+	    	</div>
+	    </div>
+		<!-- Your site ends -->
+
+        <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.2.1/js/ripples.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.2.1/js/material.min.js"></script>
+        <script src="assets/js/nbw-parallax.js"></script>
+        <script src="assets/js/jquery.localscroll-1.2.7-min.js"></script>
+        <script src="assets/js/scrollTo-1.4.2-min.js"></script>
+        <script src="assets/js/jquery.inviews.js"></script>
+        <script>
+            $(document).ready(function() {
+                $.material.init();
+                $('.navbar a').on('click', function(e) {
+
+                	//smooth scroll if inner link
+                	var to = $(this).attr('href'); // $(this) is the clicked link. We store its href.
+                	if(to.substring(0,1) == '#'){
+                		$('html, body').animate({ scrollTop: $(to).offset().top-100 }, 700);
+                	}
+                	return false;
+                });
+            });
+        </script>
+
+    </body>
+
+</html>
